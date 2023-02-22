d75ea2b19221ad4b6f9cc263fdecb1e773acffb4
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-02-22 16:45:33 +0600

    This is the random two number from integier

--- Lesson_7/lib/math.js
+++ Lesson_7/lib/math.js
@@ -5,20 +5,17 @@ Author: Siam Al Rafi
 */
 
 
+// Math object - Module scaffolding
 const math = {};
 
-
-function randomIntFromInterval(min, max) {
+// Get a random integer between two integers
+math.getRandomNumber = function getRandomNumber(min, max) {
     let minimum = min;
     let maximum = max;
     minimum = typeof minimum === 'number' ? minimum : 0;
     maximum = typeof maximum === 'number' ? maximum : 0;
     return Math.floor(Math.random() * (maximum - minimum + 1) + min);
-}
+};
 
-
-
-
-
-
-
+// Export the library
+module.exports = math;
