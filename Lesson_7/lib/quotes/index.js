a1fdae2e0f022034ba05850b6035558c09dae322
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-02-22 16:51:31 +0600

    This is the quotes file

--- Lesson_7/lib/quotes/index.js
+++ Lesson_7/lib/quotes/index.js
@@ -1,25 +1,17 @@
-/*
- * Title: Quotes Library
- * Description: Utility library for getting a list of Quotes
- */
+/* 
+Title: Quotes Library  
+Description: Quotes library is a simple library that provides random quote from my given file by using node.
+Author: Siam Al Rafi
 
-// Dependencies
-const fs = require('fs');
+*/
 
-// Quotes object - Module scaffolding
-const quotes = {};
+const quote = {};
 
-// Get all the quotes and return them to the user
-quotes.allQuotes = function allQuotes() {
-    // Read the text file containing the quotes
-    const fileContents = fs.readFileSync(`${__dirname}/quotes.txt`, 'utf8');
 
-    // Turn the string into an array
-    const arrayOfQuotes = fileContents.split(/\r?\n/);
 
-    // Return the array
-    return arrayOfQuotes;
-};
 
-// Export the library
-module.exports = quotes;
+
+
+
+module.export = quote;
+
