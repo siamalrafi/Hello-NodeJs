924148bddeed9bf4cf3a9a7a55fc14678fc03518
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-02-22 16:43:18 +0600

    This is the random number:

--- Lesson_7/lib/math.js
+++ Lesson_7/lib/math.js
@@ -1,20 +1,24 @@
-/*
- * Title: Math Library
- * Description: Utility library for math-related functions
- */
+/* 
+Title: Math Library.
+Description: Math Library is a simple library that provides random a number from between two numbers.
+Author: Siam Al Rafi
+*/
 
-// Math object - Module scaffolding
+
 const math = {};
 
-// Get a random integer between two integers
-// Inspired by: http://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
-math.getRandomNumber = function getRandomNumber(min, max) {
+
+function randomIntFromInterval(min, max) {
     let minimum = min;
     let maximum = max;
     minimum = typeof minimum === 'number' ? minimum : 0;
     maximum = typeof maximum === 'number' ? maximum : 0;
     return Math.floor(Math.random() * (maximum - minimum + 1) + min);
-};
+}
 
-// Export the library
-module.exports = math;
+
+
+
+
+
+
