dd69d7a81a24457380f63dbffcfac3e7ae77c6d7
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-02-28 17:37:37 +0600

    Thisisthe fiel

--- W3_school/index.js
+++ W3_school/index.js
@@ -3,14 +3,11 @@
 var http = require('http');
 var fs = require('fs');
 
+
 http.createServer((req, res) => {
-    fs.readFile('demofile1.html', (err, data) => {
+    fs.readFile('demofile1.html', function (err, data) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
         return res.end();
     })
-
-}).listen(8080);
-
-
-
+}).listen(8080)
