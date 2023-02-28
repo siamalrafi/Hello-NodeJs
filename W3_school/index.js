42bc543baeb543e386c7dd20e65406ab52a94afe
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-02-28 17:31:53 +0600

    Thisisthe readfiel

--- W3_school/index.js
+++ W3_school/index.js
@@ -1,1 +1,16 @@
-demo_readfile.js
+// read a file contents
+
+var http = require('http');
+var fs = require('fs');
+
+http.createServer((req, res) => {
+    fs.readFile('demofile1.html', (err, data) => {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    })
+
+}).listen(8080);
+
+
+
