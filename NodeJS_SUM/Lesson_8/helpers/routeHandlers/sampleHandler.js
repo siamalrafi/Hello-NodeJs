33e0f9b0a9681e3427025b9102f4fe0537ecfd94
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-03-08 12:46:41 +0600

    this is the new simple route

---
+++ NodeJS_SUM/Lesson_8/helpers/routeHandlers/sampleHandler.js
@@ -0,0 +1,19 @@
+/*
+ * Title: Simple handler for the application
+ * Description: A RESTFul API to monitor up or down time of user defined links
+ * Author: Sumit Saha ( Learn with Sumit )
+ * Date: 11/15/2020
+ *
+ */
+
+
+// module scaffoding 
+const handler = {};
+
+handler.simpleHandler = () => {
+    console.log("simple route");
+}
+
+module.exports = handler;
+
+
